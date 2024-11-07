64cfef2ed73c540884ffe470731e510d352760e5
ManliestBen <benjamintmanley@gmail.com>
2024-11-07 16:46:46 -0600

    complete exercise

--- src/main/java/org/exercise/Main.java
+++ src/main/java/org/exercise/Main.java
@@ -1,9 +1,6 @@
 package org.exercise;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -36,16 +33,42 @@ public class Main {
     }
 
     // TODO write to a text file
-    public static void writeFile(String fileName, String name, String result) throws IOException {
-
+    public static void writeFile(String fileName, String name, int score) throws IOException {
+        FileWriter writer = new FileWriter(fileName);
+        BufferedWriter buffer = new BufferedWriter(writer);
+        buffer.write(name + " scored " + score + "/" + superHeroes.size());
+        buffer.close();
     }
 
     // TODO write the code so the user can play the game
-    public static String playGame() {
-        return "";
+    public static int playGame(Scanner scanner) {
+        int correct = 0;
+        int numQuestions = superHeroes.size();
+        int currQuestionIdx = 0;
+
+        while (currQuestionIdx < superHeroes.size()) {
+            System.out.println();
+            String answer = getUserInput("What is " + superHeroes.get(currQuestionIdx).getSuperheroName() + "'s secret identity?", scanner);
+            System.out.println(answer + " was entered.");
+            if (answer.equalsIgnoreCase(superHeroes.get(currQuestionIdx).getRealName())) {
+                correct++;
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Incorrect!");
+            }
+            currQuestionIdx++;
+            if (currQuestionIdx < superHeroes.size()) {
+                System.out.println("Your current score is " + correct + "/" + superHeroes.size());
+            } else {
+                System.out.println("Your final score is " + correct + "/" + superHeroes.size());
+            }
+
+        }
+        return correct;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
         try {
             readFile("input.txt");
         } catch (IOException e) {
@@ -53,18 +76,25 @@ public class Main {
         }
 
         // sanity check
-        for (int i = 0; i < superHeroes.size(); i++) {
-            System.out.println(superHeroes.get(i).getSuperheroName());
-        }
+//        for (int i = 0; i < superHeroes.size(); i++) {
+//            System.out.println(superHeroes.get(i).getSuperheroName());
+//        }
 
         // TODO read the data from the user
-
-        Scanner userInput = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = userInput.nextLine();
+        String name = getUserInput("Please enter your name: ", scanner);
 
         // TODO start playing the game
+        int score;
+        score = playGame(scanner);
 
         // TODO write the results to the file
+        writeFile("output.txt", name, score);
+        scanner.close();
+    }
+
+    public static String getUserInput(String question, Scanner scanner) {
+        System.out.print(question);
+        String userInput = scanner.nextLine();
+        return userInput;
     }
 }
